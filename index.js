e5b70367ec80fb75f08691230b0fb4efcb41d0ef
JiazhenLuo <luoj278@newschool.edu>
2024-11-13 19:28:47 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express, { json } from 'express';
-import fetch from 'node-fetch';
-import cors from 'cors';
-import fs from 'fs';
+import express, { json } from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+import fs from "fs";
 
 // import movieData from './MovieData.json'
 // console.log(movieData)
@@ -12,27 +12,29 @@ app.use(express.json());
 
 const port = process.env.PORT || 3001;
 
-app.use(cors({
-    origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.get("/", (req, res) => {
-    const data = JSON.parse(fs.readFileSync('MovieData.json', 'utf-8'));
-    //console.log(data);
-    res.json(data); // Return the JSON data directly if that's what you need
+  const data = JSON.parse(fs.readFileSync("MovieData.json", "utf-8"));
+  //console.log(data);
+  res.json(data); // Return the JSON data directly if that's what you need
 });
 
-app.get('/MovieData', (req, res) => {
-    const img = req.query.img;
-    const title = req.query.title;
-    const director = req.query.director;
-    const screentime = req.query.screentime;
-    const link = req.query.link;
-    const time = req.query.time;
-    const description = req.query.Description;
-    res.json({ img, title, director, screentime, link, time, description });
+app.get("/MovieData", (req, res) => {
+  const img = req.query.img;
+  const title = req.query.title;
+  const director = req.query.director;
+  const screentime = req.query.screentime;
+  const link = req.query.link;
+  const time = req.query.time;
+  const description = req.query.Description;
+  res.json({ img, title, director, screentime, link, time, description });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
